b899f45478589f1c272b73db066893ab8857038a
unknown <ashebirdiriba8888@gmail.com>
2023-11-02 08:47:35 +0000

    updated express server

--- Building-Server-Using-Express/app.js
+++ Building-Server-Using-Express/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.end("<h1>Server Response</h1>");
+  res.end("<h1>This is Server Response</h1>");
 });
 app.listen(5000, (err) => {
   console.log("server is listening on port 5000");
