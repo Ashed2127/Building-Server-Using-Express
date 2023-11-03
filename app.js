d63ec8faaf31f83eea13ffaa701bf220b7247211
unknown <ashebirdiriba8888@gmail.com>
2023-11-03 14:35:29 +0000

    express server

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.end("<h1>  This is Server Response</h1>");
-});
+// app.get("/", (req, res) => {
+//   res.end("<h1>  This is Server Response</h1>");
+// });
+app.use(express.static("Evangadi"));
 app.listen(5000, (err) => {
-  console.log("server is listening on port 5000");
+  console.log(" listening on port 5000");
 });
